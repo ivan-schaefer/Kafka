0b81d87b23724f77dcc573d66fd405a5d42d0dbc
Ivan Schaefer <64997000+Meleeman777@users.noreply.github.com>
2024-05-21 14:24:21 +0200

    Update producer.py

--- producer.py
+++ producer.py
@@ -1,52 +1,60 @@
-import sys
-from confluent_kafka import Producer
+#!/usr/bin/env python3
 
-class ExampleProducer:
-    broker = "kafka-centos-8:29092"
-    topic = "first_topic"
-    producer = None 
-
-    def __init__(self):
-        self.producer = Producer({
-            'bootstrap.servers': self.broker,
-            'socket.timeout.ms': 100,
-            'api.version.request': 'false',
-            'broker.version.fallback': '0.9.0',
-        }
-        )
-
-    def delivery_report(self, err, msg):
-        """ Called once for each message produced to indicate delivery result.
-            Triggered by poll() or flush(). """
-        if err is not None:
-            print('Message delivery failed: {}'.format(err))
-        else:
-            print('Message delivered to {} [{}]'.format(
-                msg.topic(), msg.partition()))
-
-    def send_msg_async(self, msg):
-        print("Send message asynchronously")
-        self.producer.produce(
-            self.topic,
-            msg,
-            callback=lambda err, original_msg=msg: self.delivery_report(err, original_msg
-            ),
-        )
-        self.producer.flush()
-
-    def send_msg_sync(self, msg):
-        print("Send message synchronously")
-        self.producer.produce(
-            self.topic,
-            msg,
-            callback=lambda err, original_msg=msg: self.delivery_report(
-                err, original_msg
-            ),
-        )
-        self.producer.flush()
+import json
+from confluent_kafka.avro import AvroProducer
+from confluent_kafka import Producer, KafkaException, avro
 
 
-#SENDING DATA TO KAFKA TOPIC
-example_producer = ExampleProducer()
-message = sys.argv[1]
-example_producer.send_msg_async(message)
+
+def acked(err, msg):
+    if err is not None:
+        print("Failed to deliver message: %s: %s" % msg.value().decode('utf-8'), str(err))
+    else:
+        print("Message produced: %s" % msg.value().decode('utf-8'))
+
+
+def load_avro_schema_from_file():
+
+    key_schema = avro.load("avro/movie-topic-key.avsc")
+    value_schema = avro.load("avro/movie-topic-value.avsc")
+
+    return key_schema, value_schema
+
+
+def send_data():
+
+    producer_config = {
+        "bootstrap.servers": "kafka:29092",
+        "schema.registry.url": "http://kafka:8081"
+    }
+
+    key_schema, value_schema = load_avro_schema_from_file()
+
+    #print(key_schema)
+    #print(value_schema)
+
+    try:
+        producer = AvroProducer(producer_config, default_key_schema=key_schema, default_value_schema=value_schema)
+
+        f1 = open("data/movie-metadata.json", "r")
+        key_str = f1.read();
+        f1.close()
+
+        f2 = open("data/movie-details.json", "r")
+        value_str = f2.read()
+        f2.close()
+
+        producer.produce(topic = "movie-topic", key = json.loads(key_str), headers = [("my-header1", "Value1")], value = json.loads(value_str))
+        producer.flush()
+
+    except KafkaException as e:
+        print('Kafka failure ' + e)
+
+
+
+def main():
+    send_data()
+
+
+if __name__ == "__main__":
+    main()
